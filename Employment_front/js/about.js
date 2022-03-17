81afd79cd52407307cf566807243f1a8677c33bb
rui_1106 <lianrui@qq.com>
2022-03-17 21:11:36 +0800

    '收藏取消收藏页面完成'

--- Employment_front/js/about.js
+++ Employment_front/js/about.js
@@ -2,6 +2,7 @@ var vm = new Vue({
     el: "#app",
     data: {
         host: host,
+        token: localStorage.token,
         username: localStorage.username,
         work: {
             id: "",
@@ -11,25 +12,69 @@ var vm = new Vue({
             company: "",
             degree_required: "",
             number: "",
-            request: ""
+            request: "",
+            collection: true,
         }
     },
-    mounted(){
+    mounted() {
         this.showDetail();
     },
-    methods:{
-        showDetail(){
+    methods: {
+        showDetail() {
             var query = window.location.search.substring(1);
             console.log(query)
             axios.get(this.host + "/detail/" + query + "/", {
-                response: "json"
-            })
-            .then(response=>{
-                this.work = response.data.work;
-            })
-            .catch(error =>{
-                console.log("获取失败");
-            })
+                    headers: {
+                        'Authorization': 'JWT ' + this.token
+                    },
+                    response: "json"
+                })
+                .then(response => {
+                    this.work = response.data.work;
+                })
+                .catch(error => {
+                    console.log("获取失败");
+                })
+        },
+        Collect(id) {
+            axios.post(this.host + "/collect/", {
+                    "job_id": id,
+                }, {
+                    headers: {
+                        'Authorization': 'JWT ' + this.token
+                    },
+                    response: "json"
+                })
+                .then(response => {
+                    if (response.data.code === 0) {
+                        this.work.collection = false
+                    } else {
+                        console.log("收藏失败")
+                    }
+                })
+                .catch(error => {
+                    console.log("获取失败");
+                })
+        },
+        CancelCollect(id) {
+            axios.post(this.host + "/cancelcollect/", {
+                    "job_id": id,
+                }, {
+                    headers: {
+                        'Authorization': 'JWT ' + this.token
+                    },
+                    response: "json"
+                })
+                .then(response => {
+                    if (response.data.code === 0) {
+                        this.work.collection = true
+                    } else {
+                        console.log("取消收藏失败")
+                    }
+                })
+                .catch(error => {
+                    console.log("获取失败");
+                })
         }
     }
 })
